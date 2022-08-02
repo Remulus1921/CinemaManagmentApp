30126c22d795c9a53e8e24b291938e02fecfb369
Remigiusz Bekierz <remobekierz@onet.pl>
2022-08-02 21:40:55 +0200

    Reservation model

---
+++ CinemaManagment/Models/Reservation.cs
@@ -0,0 +1,12 @@
+﻿namespace CinemaManagment.Models
+{
+    public class Reservation
+    {
+        public int Id { get; set; }
+        public string OwnerOfReservation { get; set; }
+        public string Movie { get; set; }
+        public DateTime StartOfMovie { get; set; }
+        public int Hall { get; set; }
+        public int SeatNumber { get; set; }
+    }
+}
