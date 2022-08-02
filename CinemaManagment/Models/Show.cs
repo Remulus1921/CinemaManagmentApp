dc6c130a6f2c1afcf7a3f8245be61da5fd0dcc78
Remigiusz Bekierz <remobekierz@onet.pl>
2022-08-02 21:41:12 +0200

    Show model

---
+++ CinemaManagment/Models/Show.cs
@@ -0,0 +1,10 @@
+﻿namespace CinemaManagment.Models
+{
+    public class Show
+    {
+        public int Id { get; set; }
+        public Movie Movie { get; set; }
+        public DateTime ShowStarts { get; set; }
+        public CinemaHall Hall { get; set; }
+    }
+}
