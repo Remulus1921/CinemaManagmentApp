5841aa05720e8139d30b34e700ea97848c1b1a26
Remigiusz Bekierz <remobekierz@onet.pl>
2022-08-02 21:40:35 +0200

    CinemaHall model

---
+++ CinemaManagment/Models/CinemaHall.cs
@@ -0,0 +1,11 @@
+﻿namespace CinemaManagment.Models
+{
+    public class CinemaHall
+    {
+        public int Id { get; set; }
+        public bool IsTaken { get; set; }
+        public int HallNr { get; set; }
+        public int NrOfSeats { get; set; }
+        public Seat[] SeatsInHall { get; set; }
+    }
+}
