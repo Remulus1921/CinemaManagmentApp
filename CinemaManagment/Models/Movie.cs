91a57d4b853b40339839fa96d538d0145ab8ed00
Remigiusz Bekierz <remobekierz@onet.pl>
2022-08-02 21:40:46 +0200

    Movie model

---
+++ CinemaManagment/Models/Movie.cs
@@ -0,0 +1,10 @@
+﻿namespace CinemaManagment.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public int MovieLenght { get; set; }
+    }
+}
