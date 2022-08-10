acd165f0fbd1ef3d799df6312d66fb5a8757c55a
Remigiusz Bekierz <remobekierz@onet.pl>
2022-08-10 14:03:49 +0200

    Changed Reservation model

--- CinemaManagment/Models/Reservation.cs
+++ CinemaManagment/Models/Reservation.cs
@@ -3,10 +3,9 @@
     public class Reservation
     {
         public int Id { get; set; }
-        public string OwnerOfReservation { get; set; }
-        public string Movie { get; set; }
-        public DateTime StartOfMovie { get; set; }
-        public int Hall { get; set; }
-        public int SeatNumber { get; set; }
+        public int CreatorId { get; set; }
+        //Navigation Properties
+        public int ShowId { get; set; }
+        public Show Show { get; set; }
     }
 }
