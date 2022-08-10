a0739f7cae0585e049c3f6d15616013d2e32f5b0
Remigiusz Bekierz <remobekierz@onet.pl>
2022-08-10 14:07:41 +0200

    Changed Show model

--- CinemaManagment/Models/Show.cs
+++ CinemaManagment/Models/Show.cs
@@ -3,8 +3,14 @@
     public class Show
     {
         public int Id { get; set; }
-        public Movie Movie { get; set; }
         public DateTime ShowStarts { get; set; }
+
+        //Navigation properties
+        public int MovieId { get; set; }
+        public Movie Movie { get; set; }
+        public int CinemaHallId { get; set; }
         public CinemaHall Hall { get; set; }
+        public List<Reservation>? Reservation { get; set; }
+
     }
 }
