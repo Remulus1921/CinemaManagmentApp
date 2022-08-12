e287a58fd488f0354c8c95e9cf88aa258215c690
Remigiusz Bekierz <remobekierz@onet.pl>
2022-08-12 13:30:47 +0200

    Revert "Enable Nullable for Show"
    This reverts commit 04b9a68274f4fd97d9fbb91ea462428e40b422de.

--- CinemaManagment/Models/Show.cs
+++ CinemaManagment/Models/Show.cs
@@ -6,12 +6,11 @@
         public DateTime ShowStarts { get; set; }
 
         //Navigation properties
-        public List<Reservation?>? Reservation { get; set; }
         public int MovieId { get; set; }
-        public Movie? Movie { get; set; }
+        public Movie Movie { get; set; }
         public int CinemaHallId { get; set; }
-        public CinemaHall? Hall { get; set; }
-        
+        public CinemaHall Hall { get; set; }
+        public List<Reservation>? Reservation { get; set; }
 
     }
 }
