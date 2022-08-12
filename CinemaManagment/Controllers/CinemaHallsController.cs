3126fc26feafcf0f6641185f9274938f08469067
Remigiusz Bekierz <remobekierz@onet.pl>
2022-08-12 13:27:35 +0200

    IsTaken field removal from controler

--- CinemaManagment/Controllers/CinemaHallsController.cs
+++ CinemaManagment/Controllers/CinemaHallsController.cs
@@ -56,7 +56,7 @@ namespace CinemaManagment.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,IsTaken,HallNr,NrOfSeats")] CinemaHall cinemaHall)
+        public async Task<IActionResult> Create([Bind("Id,HallNr,NrOfSeats")] CinemaHall cinemaHall)
         {
             if (ModelState.IsValid)
             {
@@ -88,7 +88,7 @@ namespace CinemaManagment.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,IsTaken,HallNr,NrOfSeats")] CinemaHall cinemaHall)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,HallNr,NrOfSeats")] CinemaHall cinemaHall)
         {
             if (id != cinemaHall.Id)
             {
