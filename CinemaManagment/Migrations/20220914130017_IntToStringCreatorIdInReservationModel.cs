3a12ffbe4484164934612e0c98526879d62d29d6
Remigiusz Bekierz <remobekierz@onet.pl>
2022-09-14 16:27:44 +0200

    DB Migrations

---
+++ CinemaManagment/Migrations/20220914130017_IntToStringCreatorIdInReservationModel.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CinemaManagment.Migrations
+{
+    public partial class IntToStringCreatorIdInReservationModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CreatorId",
+                table: "Reservation",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "CreatorId",
+                table: "Reservation",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
