21f2eff7532f21d07adb3e94e652657b289fbd9b
Remigiusz Bekierz <remobekierz@onet.pl>
2022-09-14 16:28:30 +0200

    CreatorId int to string change

--- CinemaManagment/Models/Reservation.cs
+++ CinemaManagment/Models/Reservation.cs
@@ -3,7 +3,7 @@
     public class Reservation
     {
         public int Id { get; set; }
-        public int CreatorId { get; set; }
+        public string CreatorId { get; set; }
         //Navigation Properties
         public int ShowId { get; set; }
         public Show? Show { get; set; }
