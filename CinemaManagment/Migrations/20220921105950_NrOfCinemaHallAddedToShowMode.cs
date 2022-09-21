70fed7e535cc1d5bc8552f1a6401952f3dddf338
Remigiusz Bekierz <remobekierz@onet.pl>
2022-09-21 13:05:05 +0200

    Migrations to database

---
+++ CinemaManagment/Migrations/20220921105950_NrOfCinemaHallAddedToShowMode.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CinemaManagment.Migrations
+{
+    public partial class NrOfCinemaHallAddedToShowMode : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "NrOfCinemaHall",
+                table: "Show",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NrOfCinemaHall",
+                table: "Show");
+        }
+    }
+}
