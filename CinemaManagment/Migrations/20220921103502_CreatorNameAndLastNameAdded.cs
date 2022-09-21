1347972de717f124dce8d7ff0862d63df9a5b9c0
Remigiusz Bekierz <remobekierz@onet.pl>
2022-09-21 12:38:39 +0200

    Migration to database

---
+++ CinemaManagment/Migrations/20220921103502_CreatorNameAndLastNameAdded.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CinemaManagment.Migrations
+{
+    public partial class CreatorNameAndLastNameAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CreatorFirstName",
+                table: "Reservation",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatorLastName",
+                table: "Reservation",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatorFirstName",
+                table: "Reservation");
+
+            migrationBuilder.DropColumn(
+                name: "CreatorLastName",
+                table: "Reservation");
+        }
+    }
+}
