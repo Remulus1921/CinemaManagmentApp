5a08f450eb63dbd8f9090d5c9472c6cf0a0836bf
Remigiusz Bekierz <remobekierz@onet.pl>
2022-10-06 18:15:23 +0200

    Migrations to database

---
+++ CinemaManagment/Migrations/20221006144512_SeatsToShowAdded.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CinemaManagment.Migrations
+{
+    public partial class SeatsToShowAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Seat_CinemaHall_CinemaHallId",
+                table: "Seat");
+
+            migrationBuilder.RenameColumn(
+                name: "CinemaHallId",
+                table: "Seat",
+                newName: "ShowId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Seat_CinemaHallId",
+                table: "Seat",
+                newName: "IX_Seat_ShowId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Seat_Show_ShowId",
+                table: "Seat",
+                column: "ShowId",
+                principalTable: "Show",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Seat_Show_ShowId",
+                table: "Seat");
+
+            migrationBuilder.RenameColumn(
+                name: "ShowId",
+                table: "Seat",
+                newName: "CinemaHallId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Seat_ShowId",
+                table: "Seat",
+                newName: "IX_Seat_CinemaHallId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Seat_CinemaHall_CinemaHallId",
+                table: "Seat",
+                column: "CinemaHallId",
+                principalTable: "CinemaHall",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
